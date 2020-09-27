e2727fd0d17f47c7019add0e78ca245eea92fbfd
brianfu <fubof@sfu.ca>
2020-09-27 13:16:04 -0700

    Very tentatively got 2 api calls workking together; Must change refresh token for every new run

--- app/src/main/java/ca/sog/SOG/MainActivity.kt
+++ app/src/main/java/ca/sog/SOG/MainActivity.kt
@@ -2,14 +2,17 @@ package ca.sog.SOG
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import okio.IOException
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textViewResult: TextView
+    //private lateinit var textViewResult: TextView
+
+    var accessToken: String = ""
+    var apiServer: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +20,14 @@ class MainActivity : AppCompatActivity() {
 
         //textViewResult = findViewById(R.id.text_view_result)
         val client = OkHttpClient()
-        val refresh_token = "LdrP14aUWVc6WdG24QGLXkf-PnoZDRTO0" //TODO hardcode
+        val refresh_token = "21MjvELCdvA_XishUVIBemwnr1Tl5saT0" //TODO hardcode
         val request = Request.Builder()
-                .url("https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token=" + refresh_token).build()
+            .url("https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token=" + refresh_token)
+            .build()
 
         //https://reqres.in/api/users?page=2
-        client.newCall(request).enqueue(object : Callback {
+
+        val refresh_callback = object : Callback {
             //cannot not make http requests on mainthread
             override fun onFailure(call: Call, e: IOException) {
                 e.printStackTrace()
@@ -31,21 +36,54 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) {
                 response.use {
                     if (response.isSuccessful) {
-                        val myResponse = response.body
-                        val jsonstr = myResponse?.string() ?: "NULL"
-
+                        //val myResponse = response.body
+                        val jsonstr = response.body?.string() ?: "NULL"
+                        val json: JSONObject = JSONObject(jsonstr)
+                        accessToken = json.getString("access_token")
+                        apiServer = json.getString("api_server")
 
 
                         this@MainActivity.runOnUiThread(object : Runnable {
                             override fun run() {
-                                texter.text = jsonstr
+                                access_token.text = accessToken
                             }
 
                         })
                     }
                 }
             }
-        })
+        }
+
+        val accessTestCallback = object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                //val myResponse = response.body
+                val jsonstr = response.body?.string() ?: "NULL"
+                val json: JSONObject = JSONObject(jsonstr)
+                val account = json.getJSONArray("accounts").getJSONObject(0)
+                val accType = account.getString("type")
+                val accNum = account.getInt("number")
+
+
+                this@MainActivity.runOnUiThread(object : Runnable {
+                    override fun run() {
+                        acc_type.text = accType
+                        acc_num.text = accNum.toString()
+                    }
+                })
+            }
+        }
+
+        client.newCall(request).enqueue(refresh_callback)
+
+        val accessTestBuilder = Request.Builder().url(apiServer + "v1/accounts")
+            .header("Authorization", "Bearer " + accessToken)
+            .build()
+
+        client.newCall(accessTestBuilder).enqueue(accessTestCallback)
 
     }
 
