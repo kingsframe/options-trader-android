a41a2fabba3eb13f764f210dd98cce98d1a334d0
kai <kai@fast.co>
2020-10-04 12:28:46 -0700

    add webview and comment out networking code

--- app/src/main/java/ca/sog/SOG/MainActivity.kt
+++ app/src/main/java/ca/sog/SOG/MainActivity.kt
@@ -2,6 +2,7 @@ package ca.sog.SOG
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import okio.IOException
@@ -18,75 +19,78 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.loadUrl("https://login.questrade.com/oauth2/authorize?client_id=y4OydeHFkQTSsD1FoQBW-EOPsABwgQ&response_type=token&redirect_uri=https://www.example.com")
+
         //textViewResult = findViewById(R.id.text_view_result)
-        val client = OkHttpClient()
-        val refresh_token = "21MjvELCdvA_XishUVIBemwnr1Tl5saT0" //TODO hardcode
-
-        //https://reqres.in/api/users?page=2
-
-        val refreshCallback = object : Callback {
-            //cannot not make http requests on mainthread
-            override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                response.use {
-                    if (response.isSuccessful) {
-                        //val myResponse = response.body
-                        //TODO is this necessary
-                        val jsonstr = response.body?.string() ?: "NULL"
-                        val json: JSONObject = JSONObject(jsonstr)
-                        accessToken = json.getString("access_token")
-                        apiServer = json.getString("api_server")
-
-
-                        this@MainActivity.runOnUiThread(object : Runnable {
-                            override fun run() {
-                                access_token.text = accessToken
-                            }
-
-                        })
-                    }
-                }
-            }
-        }
-
-        val refreshRequest = Request.Builder()
-            .url("https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token=" + refresh_token)
-            .build()
-
-        client.newCall(refreshRequest).enqueue(refreshCallback)
-
-        val accessRequest = Request.Builder().url(apiServer + "v1/accounts")
-            .header("Authorization", "Bearer " + accessToken)
-            .build()
-
-        val accessCallback = object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                //val myResponse = response.body
-                //TODO look responsebody type and access json fields
-                val jsonstr = response.body?.string() ?: "NULL"
-                val json: JSONObject = JSONObject(jsonstr)
-                val account = json.getJSONArray("accounts").getJSONObject(0)
-                val accType = account.getString("type")
-                val accNum = account.getInt("number")
-
-
-                this@MainActivity.runOnUiThread(object : Runnable {
-                    override fun run() {
-                        acc_type.text = accType
-                        acc_num.text = accNum.toString()
-                    }
-                })
-            }
-        }
-
-        client.newCall(accessRequest).enqueue(accessCallback)
+//        val client = OkHttpClient()
+//        val refresh_token = "21MjvELCdvA_XishUVIBemwnr1Tl5saT0" //TODO hardcode
+//
+//        //https://reqres.in/api/users?page=2
+//
+//        val refreshCallback = object : Callback {
+//            //cannot not make http requests on mainthread
+//            override fun onFailure(call: Call, e: IOException) {
+//                e.printStackTrace()
+//            }
+//
+//            override fun onResponse(call: Call, response: Response) {
+//                response.use {
+//                    if (response.isSuccessful) {
+//                        //val myResponse = response.body
+//                        //TODO is this necessary
+//                        val jsonstr = response.body?.string() ?: "NULL"
+//                        val json: JSONObject = JSONObject(jsonstr)
+//                        accessToken = json.getString("access_token")
+//                        apiServer = json.getString("api_server")
+//
+//
+//                        this@MainActivity.runOnUiThread(object : Runnable {
+//                            override fun run() {
+//                                access_token.text = accessToken
+//                            }
+//
+//                        })
+//                    }
+//                }
+//            }
+//        }
+//
+//        val refreshRequest = Request.Builder()
+//            .url("https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token=" + refresh_token)
+//            .build()
+//
+//        client.newCall(refreshRequest).enqueue(refreshCallback)
+//
+//        val accessRequest = Request.Builder().url(apiServer + "v1/accounts")
+//            .header("Authorization", "Bearer " + accessToken)
+//            .build()
+//
+//        val accessCallback = object : Callback {
+//            override fun onFailure(call: Call, e: IOException) {
+//                e.printStackTrace()
+//            }
+//
+//            override fun onResponse(call: Call, response: Response) {
+//                //val myResponse = response.body
+//                //TODO look responsebody type and access json fields
+//                val jsonstr = response.body?.string() ?: "NULL"
+//                val json: JSONObject = JSONObject(jsonstr)
+//                val account = json.getJSONArray("accounts").getJSONObject(0)
+//                val accType = account.getString("type")
+//                val accNum = account.getInt("number")
+//
+//
+//                this@MainActivity.runOnUiThread(object : Runnable {
+//                    override fun run() {
+//                        acc_type.text = accType
+//                        acc_num.text = accNum.toString()
+//                    }
+//                })
+//            }
+//        }
+//
+//        client.newCall(accessRequest).enqueue(accessCallback)
 
     }
 
