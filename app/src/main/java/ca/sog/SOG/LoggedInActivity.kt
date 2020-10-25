a95fde88cd4882ea168b945a5d322dc707d8509f
kai <kai@fast.co>
2020-10-25 12:14:24 -0700

    fix text view issue

--- app/src/main/java/ca/sog/SOG/LoggedInActivity.kt
+++ app/src/main/java/ca/sog/SOG/LoggedInActivity.kt
@@ -1,17 +1,15 @@
 package ca.sog.SOG
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_logged_in.*
 
-
 class LoggedInActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logged_in)
         val intent = intent
         val url = intent.extras?.getString("tokenURL")
-        url_text.text = "hello world"
-
+        textView.text = url
     }
 }
