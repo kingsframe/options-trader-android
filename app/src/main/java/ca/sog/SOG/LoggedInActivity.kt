9d0e6ad3e999acda764107e22a1699abbd03ffd6
kai <kai@fast.co>
2020-11-29 10:28:53 -0800

    rename tokens list

--- app/src/main/java/ca/sog/SOG/LoggedInActivity.kt
+++ app/src/main/java/ca/sog/SOG/LoggedInActivity.kt
@@ -10,14 +10,13 @@ class LoggedInActivity : AppCompatActivity() {
         setContentView(R.layout.activity_logged_in)
 
         val tokenBundle = intent.extras
-        val partsList = tokenBundle?.getStringArrayList("tokens") ?: ArrayList<String>()
+        val tokensList = tokenBundle?.getStringArrayList("tokens") ?: ArrayList<String>()
 
-        //val intent = intent
-        val access_token = partsList[0]
-        val refresh_token = partsList[1]
-        val token_type = partsList[2]
-        val expires_in = partsList[3]
-        val api_server = partsList[4]
+        val access_token = tokensList[0]
+        val refresh_token = tokensList[1]
+        val token_type = tokensList[2]
+        val expires_in = tokensList[3]
+        val api_server = tokensList[4]
         textView.text = "accesstoken = " + access_token + "  rt=" + refresh_token + token_type + " expire_in= " + expires_in + "  api_serv = " + api_server
     }
 }
