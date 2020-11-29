a9dc57c281881e6cb036e095db68747e4a667d71
kai <kai@fast.co>
2020-11-29 12:01:24 -0800

    fail networking call to get accounts

--- app/src/main/java/ca/sog/SOG/LoggedInActivity.kt
+++ app/src/main/java/ca/sog/SOG/LoggedInActivity.kt
@@ -2,6 +2,8 @@ package ca.sog.SOG
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import okhttp3.*
+import okio.IOException
 import kotlinx.android.synthetic.main.activity_logged_in.*
 
 class LoggedInActivity : AppCompatActivity() {
@@ -17,12 +19,26 @@ class LoggedInActivity : AppCompatActivity() {
         val token_type = tokensList[2]
         val expires_in = tokensList[3]
         val api_server = tokensList[4]
-        textView.text = "accesstoken = " + access_token + "  rt=" + refresh_token + token_type + " expire_in= " + expires_in + "  api_serv = " + api_server
+//        textView.text = "accesstoken = " + access_token + "  rt=" + refresh_token + token_type + " expire_in= " + expires_in + "  api_serv = " + api_server
 
 //         TODO make networking call to
 //        GET /v1/accounts HTTP/1.1
 //        Host: https://api01.iq.questrade.com
 //        Authorization: Bearer C3lTUKuNQrAAmSD/TPjuV/HI7aNrAwDp
+
+        val client = OkHttpClient()
+        val request = Request.Builder()
+                .url(api_server + "/v1/accounts")
+                .header("Authorization", "Bearer " + access_token)
+                .build()
+        try {
+            val response = client.newCall(request).execute()
+            textView.text = response.body.toString()
+        } catch (ex: Exception) {
+            textView.text = "Sorry Error fetch accounts info"
+        }
+
+
 
 //        TODO decide either to make List UI for accounts response OR
 //        select the first account to call accounts/:id/positions
