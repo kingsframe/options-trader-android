5706136bb0fcfd80dcbcc6d4a87a74dbcfc2550a
atangzer <ayatng@sfu.ca>
2020-12-31 00:45:07 -0800

    added client for PositionsActivity (WIP)

--- app/src/main/java/ca/sog/SOG/PositionsActivity.kt
+++ app/src/main/java/ca/sog/SOG/PositionsActivity.kt
@@ -1,8 +1,17 @@
 package ca.sog.SOG
 
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.account_item.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONObject
+import java.io.IOException
 
 
 class PositionsActivity : AppCompatActivity() {
@@ -12,9 +21,43 @@ class PositionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_positions)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        val accountNumber = intent.getStringExtra("accountNumber")
+        val tokenBundle = intent.extras
+        val tokensList = tokenBundle?.getStringArrayList("tokens") ?: ArrayList<String>()
+
+        val access_token = tokensList[0]
+        val api_server = tokensList[4]
+
+        val symbol: TextView = findViewById((R.id.sym))
+        val symbolId: TextView = findViewById((R.id.symId))
 
         Toast.makeText(this,"account number ${accountNumber}",Toast.LENGTH_LONG).show()
+
+        // GET https://api01.iq.questrade.com/v1/accounts/26598145/positions
+
+        // why GlobalScope.launch?
+        GlobalScope.launch {
+            val client = OkHttpClient()
+            val request = Request.Builder()
+                    .url(api_server + "v1/accounts/${accountNumber}")
+                    .header("Authorization", "Bearer $access_token")
+                    .build()
+
+            lateinit var responseBodyJson: JSONObject
+            client.newCall(request).execute().use{ response ->
+                if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                response.use {
+                    val responseBody = response.body
+                    val responseBodyString = responseBody?.string() ?: "NULL"
+                    responseBodyJson = JSONObject(responseBodyString)
+                    val gson = GsonBuilder().create()
+                }
+
+                symbol.text = response.toString()
+                symbolId.text = "Hello World!"
+            }
+
+        }
 
     }
 }
