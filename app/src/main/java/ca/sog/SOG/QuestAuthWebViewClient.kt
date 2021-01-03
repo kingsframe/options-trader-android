603d3b68706b9c1eafcf91da81d7e63048c2680f
atangzer <ayatng@sfu.ca>
2021-01-03 11:26:14 -0800

    updates to PositionActivity

--- app/src/main/java/ca/sog/SOG/QuestAuthWebViewClient.kt
+++ app/src/main/java/ca/sog/SOG/QuestAuthWebViewClient.kt
@@ -21,9 +21,13 @@ class QuestAuthWebViewClient(private val context: Context) : WebViewClient() {
         tokenBundle.putStringArrayList("tokens", tokens.getArray())
 
         val startLoggedInActivityIntent = Intent(context, AccountsActivity::class.java)
+        val startLoggedInPositionsIntent = Intent(context, PositionsActivity::class.java)
+
         startLoggedInActivityIntent.putExtras(tokenBundle)
+        startLoggedInPositionsIntent.putExtras(tokenBundle)
 
         startActivity(context, startLoggedInActivityIntent, tokenBundle)
+        startActivity(context, startLoggedInPositionsIntent, tokenBundle)
 
         return true
     }
