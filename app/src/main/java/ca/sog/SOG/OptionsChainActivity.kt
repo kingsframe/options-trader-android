67d16bcd2529b8cd5d1f6d0edd228542a3c2de81
atangzer <ayatng@sfu.ca>
2021-02-06 19:22:11 -0800

    Revert "WIP:OptionsChainActivity"
    This reverts commit 8049b2ad8a3417903e10353dac6a35ba8d36580d.

--- app/src/main/java/ca/sog/SOG/OptionsChainActivity.kt
+++ app/src/main/java/ca/sog/SOG/OptionsChainActivity.kt
@@ -1,28 +1,8 @@
 package ca.sog.SOG
 
-import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_ticker_search.*
 
-class OptionsChainActivity: AppCompatActivity(), OnItemClickListener{
-    lateinit var responseOptionsList : MutableList<Options>
-    //lateinit var optionsAdapter = OptionsAdapter
-
-    val symbolInfo = intent.extras
-    val symbolId = symbolInfo?.getString("symbolId") ?: String()
-    val symbolName = symbolInfo?.getString("symbolName") ?: String()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Showing results for ${symbolName}", Toast.LENGTH_LONG).show()
-    }
-
-    override fun onItemClicked(symbolId: Int, symbolName: String) {
-        TODO("Not yet implemented")
-    }
-
-
-    //GET https://api01.iq.questrade.com/v1/symbols/9291/options
-
-}
+//class OptionsIDActivity: AppCompatActivity(), OnItemClickListener {
+//    lateinit var responseList : MutableList<Options>
+//    lateinit var tickerAdapter : OptionsIDAdapter
+//}
